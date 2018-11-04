6b2c91670bb36a770bb15b7a8ee3a75a950dd724
Scott Willeke <scott@willeke.com>
2018-11-04 01:13:50 -0800

    yup + react-formal forms.

--- demo/web/pages/futureEarnings.tsx
+++ demo/web/pages/futureEarnings.tsx
@@ -4,6 +4,8 @@ import Layout from "../components/Layout"
 import { NextContext } from "next"
 import { BigNumber } from "bignumber.js"
 import IntlMessageFormat from "intl-messageformat"
+import Form from "react-formal"
+import * as yup from "yup"
 
 import {
   buildBaseUrlForApi,
@@ -41,58 +43,61 @@ class MyPage extends React.Component<MyProps, MyState> {
 
   render = () => {
     console.log("render state:", this.state)
+    const modelSchema = this.buildFormSchema()
+    const form = (
+      <Form schema={modelSchema} defaultValue={modelSchema.default()}>
+        <div className="form-group">
+          <label>
+            Time Horizon
+            <Form.Field name="timeHorizonInDays" className="form-control" />
+          </label>
+          <Form.Message for="timeHorizonInDays" />
+        </div>
+
+        <div className="form-group">
+          <label>
+            Your Hashes Per Second
+            <Form.Field name="yourHashesPerSecond" className="form-control" />
+          </label>
+          <Form.Message for="yourHashesPerSecond" />
+        </div>
+
+        <div className="form-group">
+          <label>
+            Watts
+            <Form.Field name="watts" className="form-control" />
+          </label>
+          <Form.Message for="watts" />
+        </div>
+
+        <div className="form-group">
+          <label>
+            Fiat Exchange Rate
+            <Form.Field
+              name="fiatPerCoinsExchangeRate"
+              className="form-control"
+            />
+          </label>
+          <Form.Message for="fiatPerCoinsExchangeRate" />
+        </div>
+
+        <div className="form-group">
+          <label>
+            Electricity Cost (kWh)
+            <Form.Field name="electricityCostKwh" className="form-control" />
+          </label>
+          <Form.Message for="electricityCostKwh" />
+        </div>
+
+        <Form.Button type="submit" className="btn btn-primary">
+          Submit
+        </Form.Button>
+      </Form>
+    )
     return (
       <Layout>
         <p>hi futureEarnings</p>
-        <form style={formStyle} onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label>
-              Electricity Cost (per kWh):
-              <input
-                id="electricityCostKwh"
-                className="form-control"
-                type="number"
-                min="0.01"
-                max="0.5"
-                step="0.01"
-                value={
-                  this.state && this.state.electricityCostKwh
-                    ? this.state.electricityCostKwh
-                    : 0
-                }
-                onChange={event =>
-                  event.target && event.target.value
-                    ? this.setState({
-                        electricityCostKwh: parseFloat(event.target.value)
-                      })
-                    : null
-                }
-              />
-            </label>
-          </div>
-          <div className="form-group">
-            <label>Email address</label>
-            <input
-              type="email"
-              className="form-control"
-              placeholder="Enter email"
-            />
-          </div>
-          <div className="form-group">
-            <label>
-              Email address2
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Enter email"
-              />
-            </label>
-          </div>
-
-          <div>
-            <input type="submit" value="BOOM" />
-          </div>
-        </form>
+        {form}
         <p>
           meanNetworkSecondsBetweenBlocks:{" "}
           {this.formattedSecondsBetweenBlocks()}
@@ -151,22 +156,66 @@ class MyPage extends React.Component<MyProps, MyState> {
     }
   }
 
-  buildOptions() {
-    /*
-    let opt: EstimateFutureEarningsOptions = {
-      electricityCostKwh: 0.11,
-      feesAsPercent: 0.01,
-      fiatPerCoinsExchangeRate: 115,
-      meanNetworkSecondsBetweenBlocks: ...fetch,
-      networkHashesPerSecond: ...fetch,
-      networkHashRateChangePerDay: ...fetch,
-      rewardedCoinsPerMinedBlock: 10,
-      timeHorizonInDays: -input-,
-      watts: -input-,
-      yourHashesPerSecond: -input-
+  buildOptions(): EstimateFutureEarningsOptions {
+    const options: EstimateFutureEarningsOptions = {
+      // TODO: FORM
+      timeHorizonInDays: null,
+      // TODO: FORM
+      yourHashesPerSecond: new BigNumber(290),
+      // TODO: FETCH
+      networkHashesPerSecond: new BigNumber(492451309),
+      // TODO: FETCH
+      meanNetworkSecondsBetweenBlocks: 147.29166,
+      // static; future fetch.
+      rewardedCoinsPerMinedBlock: 10, // is actually 12.5, but 2.5 is "founder reward"
+      // TODO: FETCH
+      fiatPerCoinsExchangeRate: 241.23,
+      // TODO: FORM
+      watts: 190,
+      // TODO: FORM
+      electricityCostKwh: 0.1,
+      // TODO: FETCH
+      networkHashRateChangePerDay: new BigNumber(0),
+      // TODO: FORM
+      feesAsPercent: 0.01
     }
-    return opt
-    */
+    return options
+  }
+
+  buildFormSchema() {
+    return yup.object({
+      timeHorizonInDays: yup
+        .number()
+        .default(7)
+        .label("Time Horizon for how far out to estimate earnings")
+        .required(
+          "please provide a time horizon for how far out to estimate earnings"
+        )
+        .positive(),
+      yourHashesPerSecond: yup
+        .number()
+        .default(280)
+        .label("Your hash rate in GigaHashes per second")
+        .required()
+        .positive(),
+      watts: yup
+        .number()
+        .default(150)
+        .label("Your hash rate in GigaHashes per second")
+        .required()
+        .positive(),
+      fiatPerCoinsExchangeRate: yup
+        .number()
+        .default(150) // TODO: FETCH!
+        .label("Fiat Exchange Rate")
+        .required(
+          "Please enter Fiat Exchange Rate (the exchange rate between ZCash and your fiat currency)"
+        ),
+      electricityCostKwh: yup
+        .number()
+        .default(0.11)
+        .label("Electricity Cost")
+    })
   }
 }
 
